a9d0def8c5ccfe154d661ef155fc29e3bddb7b1d
Pablo Gaston Lopez Krohn <plopez_krohn@hotmail.com>
2022-11-30 01:00:28 -0300

    Lista Alumnos

--- src/app/model/alumno.ts
+++ src/app/model/alumno.ts
@@ -8,19 +8,4 @@ export class Alumno {
     this.nombre = firstName;
     this.clase = lastName;
   }
-
-  prueba() {
-    if (this.clase === 'Delantero') {
-      let clases = document.getElementById('clase-color');
-      clases?.classList.add('list-group-item-danger');
-    } else if (this.clase === 'Defensor') {
-      alert('asd');
-    } else if (this.clase === 'Mediocampo') {
-      alert('asdd');
-    } else if (this.clase === 'G.O.A.T') {
-      alert('asdaaaaa');
-    } else if (this.clase === 'Arquero') {
-      alert('asd');
-    }
-  }
 }
